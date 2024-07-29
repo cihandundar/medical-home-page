589df8a36208f58342f6abd78f54a6d289bf88fe
cihandundar <94462152+cihandundar@users.noreply.github.com>
2024-07-29 15:24:28 +0300

    fix

--- script.js
+++ script.js
@@ -46,56 +46,50 @@ var swiper = new Swiper(".news-swiper", {
 });
 
 // DOCTOR SLIDER
-const container = document.querySelector(".slider-container");
-const sliderTrack = document.querySelector(".slider-track");
-const doctorBoxes = document.querySelectorAll(".box");
-const circles = document.querySelectorAll(".circle-item");
 
-let currentIndex = 0;
-
-function getItemsPerPage() {
-  const width = window.innerWidth;
-  if (width <= 480) {
-    return 1;
-  } else if (width <= 768) {
-    return 2;
-  } else {
-    return 3;
-  }
-}
-
-function updateSlider() {
-  const itemsPerPage = getItemsPerPage();
-  const boxWidth = container.offsetWidth / itemsPerPage;
-  sliderTrack.style.transform = `translateX(-${currentIndex * boxWidth}px)`;
-
-  circles.forEach((circle, i) => {
-    if (i === currentIndex / itemsPerPage) {
-      circle.classList.add("active");
-    } else {
-      circle.classList.remove("active");
-    }
-  });
-}
-
-circles.forEach((circle, index) => {
-  circle.addEventListener("click", () => {
-    currentIndex = index * getItemsPerPage();
-    updateSlider();
-  });
-});
-
-window.addEventListener("resize", updateSlider);
-
-updateSlider();
-
-// HERO SLIDER
-var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+var swiper = new Swiper(".slider-track", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
   },
 });
+// DOCTOR SLIDER
+
+var swiper = new Swiper(".services-right-card-wrapper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+services - right - card - wrapper;
 
 // ACCORDION MENU
 const faqs = document.querySelectorAll(".faq");
